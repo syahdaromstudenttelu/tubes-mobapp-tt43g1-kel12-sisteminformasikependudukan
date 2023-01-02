d631c98020e8e32d2129c54ab15a72e512967d11
syahdaromstudenttelu <syahdarom@student.telkomuniversity.ac.id>
2023-01-02 10:14:28 +0700

    chore(project): initialize project setup

---
+++ app/src/main/java/ac/telkomuniversity/mobapp/tt43g1/kel12/sisteminformasikependudukan/MainActivity.kt
@@ -0,0 +1,11 @@
+package ac.telkomuniversity.mobapp.tt43g1.kel12.sisteminformasikependudukan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
